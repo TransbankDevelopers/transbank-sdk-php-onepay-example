d1f9073a6d49b4f29e4a5fddeb506725886986e6
Gonzalo Castillo <gonzalo.castillo@continuum.cl>
2018-09-12 23:24:27 -0300

    Update js lib version to 1.5.1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -517,7 +517,7 @@
 <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <!--===============================================================================================-->
 <script src="js/main.js"></script>
-<script src="https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v1.5.0/lib/merchant.onepay.min.js"></script>
+<script src="https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v1.5.1/lib/merchant.onepay.min.js"></script>
 <script src="js/use-onepay.js"></script>
 
 <div>
