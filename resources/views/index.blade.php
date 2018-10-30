2a6cdc02909dea8816de08237b76cefa96f5e0bf
Victor Utreras J <vutreras@live.com>
2018-10-30 15:34:11 -0300

    Update sdk to v1.5.8

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -517,7 +517,7 @@
 <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
 <!--===============================================================================================-->
 <script src="js/main.js"></script>
-<script src="https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v1.5.3/lib/merchant.onepay.min.js"></script>
+<script src="https://cdn.rawgit.com/TransbankDevelopers/transbank-sdk-js-onepay/v1.5.8/lib/merchant.onepay.min.js"></script>
 <script src="js/use-onepay.js"></script>
 
 <div>
